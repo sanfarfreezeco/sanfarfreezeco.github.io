6f0096dd7dc790c4607e82a3cc8190dc95d863a2
sanfarfreezeco <farizcocoa@live.com>
2024-07-14 15:06:54 +0700

    Add "Ongoing" and Fix underline

---
+++ js/header.js
@@ -0,0 +1,7 @@
+// Underline Page on Header //
+
+if (window.location.pathname === "/") {
+    document.getElementById("work-u").style.opacity = "1";
+} else if (window.location.pathname === "/aboutme") {
+    document.getElementById("aboutme-u").style.opacity = "1";
+}
