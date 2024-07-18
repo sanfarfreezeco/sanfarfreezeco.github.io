757fda8700773da790f589cb167edae9be22da6d
sanfarfreezeco <farizcocoa@live.com>
2024-07-18 21:57:01 +0700

    Fix header

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@
 window.onload = function () {
     if (window.location.pathname === "/") {
         document.getElementById("work-u").style.opacity = "1";
-    } else if (window.location.pathname === "/aboutme") {
+    } else if (window.location.pathname === "/aboutme/") {
         document.getElementById("aboutme-u").style.opacity = "1";
     }
 }
