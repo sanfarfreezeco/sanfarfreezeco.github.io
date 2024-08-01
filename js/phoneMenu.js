95153bde9b09d2a6d8153956cf76209102cc7a1a
sanfarfreezeco <farizcocoa@live.com>
2024-08-01 22:30:48 +0700

    Fix layout on phone

--- js/phoneMenu.js
+++ js/phoneMenu.js
@@ -1,7 +1,9 @@
 function openMenu() {
     document.getElementById("menu-screen-phone").style.width = "100%";
+    document.body.style.overflowY = "hidden";
 }
 
 function closeMenu() {
     document.getElementById("menu-screen-phone").style.width = "0";
+    document.body.style.overflowY = "initial";
 }
