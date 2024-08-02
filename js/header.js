71cff091591f948afe42c6771e4ddbd8a7484a45
sanfarfreezeco <farizcocoa@live.com>
2024-08-02 20:32:47 +0700

    Fix code for iPhone and add CSS code for webkit and ms

--- js/header.js
+++ js/header.js
@@ -2,15 +2,18 @@ if (document.location.pathname === "/aboutme/") {
     window.addEventListener("DOMContentLoaded", () => {
         document.getElementById("header-container").style.backgroundColor = "rgba(255, 250, 240, 0.75)";
         document.getElementById("header-container").style.boxShadow = "0 0 20px grey";
+        document.getElementById("header-container").style.webkitBoxShadow = "0 0 20px grey";
     });
 } else {
     window.addEventListener("scroll", () => {
         if (window.pageYOffset >= 50) {
             document.getElementById("header-container").style.backgroundColor = "rgba(255, 250, 240, 0.75)";
             document.getElementById("header-container").style.boxShadow = "0 0 20px grey";
+            document.getElementById("header-container").style.webkitBoxShadow = "0 0 20px grey";
         } else {
             document.getElementById("header-container").style.backgroundColor = "floralwhite";
             document.getElementById("header-container").style.boxShadow = "initial";
+            document.getElementById("header-container").style.webkitBoxShadow = "initial";
         }
     });
 }
